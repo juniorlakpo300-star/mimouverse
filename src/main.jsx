f60e946329fd96fe7b02316f02918764fb1f4820
juniorlakpo300-star <juniorlakpo300@gmail.com>
2026-09-05 03:45:15 +0000

    Activer la nouvelle couche visuelle globale MIMOUVERSE

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import './participation-admin.css'
 import './participation-redesign.css'
 import './publication-redesign.css'
 import './admin-gate.css'
+import './visual-overhaul.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
